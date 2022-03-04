0f9cad7e4102bb0404c42252fe9d948051b68d6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-04 19:58:30 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-28-generic #31~20.04.1-Ubuntu SMP Wed Jan 19 14:08:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:58:30 up 1 day, 23:54,  1 user,  load average: 0.13, 0.12, 0.14

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -13,7 +13,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
-IMG ?=
+IMG ?= "nemu_trap"
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
