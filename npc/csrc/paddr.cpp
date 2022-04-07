6fd8a4f5c0131a3d1d1dc769408d3faea571f8e0
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 13:46:37 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:46:37 up 2 days,  4:03,  1 user,  load average: 0.29, 0.24, 0.22

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -11,16 +11,16 @@
 u_int32_t memery(u_int64_t addr){
     //u_int32_t *inst = (u_int64_t *)addr;
 //    std::cout << *inst << addr << (int *)0x80000000 << std::endl;
-    return *(u_int64_t *)addr;//*inst;
-    // switch (addr)
-    // {
-    // case 0x80000000: return 0xffc10113; break;
-    // case 0x80000004: return 0xff010113; break;
-    // case 0x80000008: return 0x00000517; break;
-    // case 0x8000000c: return 0x01c50513; break;
-    // case 0x80000010: return 0x00100073; break;
+ //   return *(u_int64_t *)addr;//*inst;
+     switch (addr)
+     {
+     case 0x80000000: return (u_int64_t *)0x80000000; break;
+     case 0x80000004: return 0xff010113; break;
+     case 0x80000008: return 0x00000517; break;
+     case 0x8000000c: return 0x01c50513; break;
+     case 0x80000010: return 0x00100073; break;
     
-    // default: return 0; break;
-    // }
+     default: return 0; break;
+     }
 }
 
