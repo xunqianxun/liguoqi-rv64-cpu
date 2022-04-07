195a7579f9793405438729eb37bfd5a3e11b1973
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 21:23:32 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:32 up 2 days, 11:40,  1 user,  load average: 0.41, 0.29, 0.11

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <verilated.h> 
 
-// u_int64_t *returnaddr(u_int64_t addr){ return addr;}
-
-// u_int64_t mem_rade(void *addr){
-//     return *(u_int64_t *)addr;
+// u_int32_t memery(u_int64_t addr){
+//      switch (addr)
+//      {
+//      case 0x80000000: return 0x01c50513; break;
+//      case 0x80000004: return 0xff010113; break;
+//      case 0x80000008: return 0x00000517; break;
+//      case 0x8000000c: return 0x01c50513; break;
+//      case 0x80000010: return 0x00000517; break;
+    
+//      default: return 0; break;
+//      }
 // }
 
+int getBinSize(std::string path){
+    int size = 0;
+    std::ifstream infile(path, std::ifstream::binary);
+    infile.seekg(0, infile.end);
+    int size = infile.tellg();
+    infile.seekg(0, infile.beg);
 
-u_int32_t memery(u_int64_t addr){
-    //u_int32_t *inst = (u_int64_t *)addr;
-//    std::cout << *inst << addr << (int *)0x80000000 << std::endl;
- //   return *(u_int64_t *)addr;//*inst;
-     switch (addr)
-     {
-     case 0x80000000: return 0x01c50513; break;
-     case 0x80000004: return 0xff010113; break;
-     case 0x80000008: return 0x00000517; break;
-     case 0x8000000c: return 0x01c50513; break;
-     case 0x80000010: return 0x00000517; break;
-    
-     default: return *(u_int64_t *)0x80000010; break;
-     }
+    infile.close();
+    return siez;
 }
 
