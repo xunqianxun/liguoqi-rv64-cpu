fae5ff391eb3e06b134d4123b7427c84876fcd0b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 15:36:26 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:26 up 8 days,  5:53,  1 user,  load average: 1.03, 1.16, 1.06

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 string filepach = "/home/mulin/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
 int load_img_getsize(string path);
-void readbin(string path, char * buf, int size);
+void readbin(string path, uint8_t * buf, int size);
 static void img_load();
 void init_monitorp(int argc, char *argv[]);
 
-static char pmem[0x8000000] __attribute((aligned(4096)))  = {};
+static uint8_t pmem[0x8000000] __attribute((aligned(4096)))  = {};
 
 int load_img_getsize(string path){
   int size = 0;
@@ -22,12 +22,13 @@ int load_img_getsize(string path){
   return size;
 }
 
-void readbin(string path, char * buf, int size){
-//   char *buff ;
-//   (char *)buf = buff;
+void readbin(string path, uint8_t * buf, int size){
+  char *buff[0x8000000] ;
+  (char *)buf = buff;
   ifstream infile(path, ifstream::binary);
-  infile.read(buf, size);
+  infile.read(buff, size);
   infile.close();
+  cout << buff[1] << endl;
 }
 
 // string returnpach(string filepacht){
--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -39,7 +39,7 @@ uint32_t ifetch(uint64_t addr, int len){
 }
 
 
-char *gi_to_hi(uint32_t addr) {return pmem + addr - 0x8000000;}
+uint8_t *gi_to_hi(uint32_t addr) {return pmem + addr - 0x8000000;}
 uint32_t hi_to_gi(uint8_t *addr) {return addr - pmem + 0x8000000;}
 
 static uint64_t pmem_read(uint32_t addr, int len) {
