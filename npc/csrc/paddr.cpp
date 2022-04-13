39716fe7582ac2c6417e484600defe56b690477a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 20:47:46 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:46 up 8 days, 11:05,  1 user,  load average: 0.58, 0.54, 0.54

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -41,6 +41,7 @@ uint32_t ifetch(uint64_t addr, int len){
 
 uint64_t paddr_read(uint32_t addr, int len) {
   return pmem_read(addr, len);
+}
 //   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
 //   out_of_bound(addr);
 //   return 0;
