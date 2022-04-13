db2315bb795d3070df41484d1f021c4fb6b60a85
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 19:20:45 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:20:45 up 8 days,  9:38,  1 user,  load average: 0.41, 0.59, 0.43

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,7 +19,7 @@ double sc_time_stamp(){
 }
 
 int main(int argc , char** argv , char** env) {
-img_load();
+load_img();
 VerilatedContext* contextp = new VerilatedContext ;
 contextp->commandArgs(argc, argv) ;
 Vrvcpu* rvcpu = new Vrvcpu(contextp);
