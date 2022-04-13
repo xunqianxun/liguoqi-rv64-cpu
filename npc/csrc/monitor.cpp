fcf0358c0cf1cd790a7b037b7ba3958d3075f3f2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 19:13:13 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:13 up 8 days,  9:30,  1 user,  load average: 0.63, 0.52, 0.30

--- npc/csrc/monitor.cpp
+++ npc/csrc/monitor.cpp
@@ -2,42 +2,64 @@
 #include<fstream>
 #include"sdb.cpp"
 
-using namespace std;
+// using namespace std;
 
-string filepach = "/home/mulin/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
-int load_img_getsize(string path);
-void readbin(string path, uint8_t * buf, int size);
-static void img_load();
-void init_monitorp(int argc, char *argv[]);
+// string filepach = "/home/mulin/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
+// int load_img_getsize(string path);
+// void readbin(string path, uint8_t * buf, int size);
+// static void img_load();
+// void init_monitorp(int argc, char *argv[]);
 
-static uint8_t pmem[0x8000000] __attribute((aligned(4096)))  = {};
+ static uint8_t pmem[0x8000000] __attribute((aligned(4096)))  = {};
 
-int load_img_getsize(string path){
-  int size = 0;
-  ifstream infile(path, ifstream::binary);
-  infile.seekg(0, infile.end);
-  size = infile.tellg();
-  infile.seekg(0, infile.beg);
-  infile.close();
-  return size;
-}
+// int load_img_getsize(string path){
+//   int size = 0;
+//   ifstream infile(path, ifstream::binary);
+//   infile.seekg(0, infile.end);
+//   size = infile.tellg();
+//   infile.seekg(0, infile.beg);
+//   infile.close();
+//   return size;
+// }
 
-void readbin(string path, uint8_t * buf, int size){
-  ifstream infile(path, ifstream::binary);
-  infile.read(reinterpret_cast<char *> (buf), size);
-  for(int count; count < size; count++)
-  cout << buf[count] << endl;
-  infile.close();
-}
+// void readbin(string path, uint8_t * buf, int size){
+//   ifstream infile(path, ifstream::binary);
+//   infile.read(reinterpret_cast<char *> (buf), size);
+//   for(int count; count < size; count++)
+//   cout << buf[count] << endl;
+//   infile.close();
+// }
 
 // string returnpach(string filepacht){
 //     return filepacht;
 // }
 
-static void img_load(){
-    // string filepach = "/home/mulin/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
-    int size = load_img_getsize(filepach);
-    readbin(filepach, pmem, size);
+// static void img_load(){
+//     int size = load_img_getsize(filepach);
+//     readbin(filepach, pmem, size);
+// }
+
+static long load_img() {
+  char img_file = "/home/mulin/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin"
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  Log("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(gi_to_hi(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
 }
 
 void init_monitorp(int argc, char *argv[]){
