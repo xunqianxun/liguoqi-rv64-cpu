e8be844aae5d2c80fb2a7fed9e302a863dc5be94
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 17:30:38 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:30:38 up 12 days,  7:47,  1 user,  load average: 0.38, 0.55, 0.46

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -3,67 +3,27 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-// static void csr_prepare() {
-//   cpu.mstatus = mstatus->val;
-//   cpu.mcause  = mcause->val;
-//   cpu.mepc    = mepc->val;
-
-//   cpu.sstatus = csrid_read(0x100); // sstatus
-//   cpu.scause  = scause->val;
-//   cpu.sepc    = sepc->val;
-
-//   cpu.satp     = satp->val;
-//   cpu.mip      = mip->val;
-//   cpu.mie      = mie->val;
-//   cpu.mscratch = mscratch->val;
-//   cpu.sscratch = sscratch->val;
-//   cpu.mideleg  = mideleg->val;
-//   cpu.medeleg  = medeleg->val;
-//   cpu.mtval    = mtval->val;
-//   cpu.stval    = stval->val;
-//   cpu.mtvec    = mtvec->val;
-//   cpu.stvec    = stvec->val;
-// }
-
-// static void csr_writeback() {
-//   mstatus->val = cpu.mstatus;
-//   mcause ->val = cpu.mcause ;
-//   mepc   ->val = cpu.mepc   ;
-//   //sstatus->val = cpu.sstatus;  // sstatus is a shadow of mstatus
-//   scause ->val = cpu.scause ;
-//   sepc   ->val = cpu.sepc   ;
-
-//   satp->val     = cpu.satp;
-//   mip->val      = cpu.mip;
-//   mie->val      = cpu.mie;
-//   mscratch->val = cpu.mscratch;
-//   sscratch->val = cpu.sscratch;
-//   mideleg->val  = cpu.mideleg;
-//   medeleg->val  = cpu.medeleg;
-//   mtval->val    = cpu.mtval;
-//   stval->val    = cpu.stval;
-//   mtvec->val    = cpu.mtvec;
-//   stvec->val    = cpu.stvec;
-// }
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+//  assert(0);
+  if (direction == DIFFTEST_TO_REF) {memcpy(guest_to_host(addr), buf, n);}
+  else {memcpy(buf, guest_to_host(addr), n);}
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
-  // if (direction == DIFFTEST_TO_REF) {
-  //   memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
-  //   csr_writeback();
-  // } else {
-  //   csr_prepare();
-  //   memcpy(dut, &cpu, DIFFTEST_REG_SIZE);
-  // }
+//  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
+//    csr_writeback();
+  } else {
+//    csr_prepare();
+    memcpy(dut, &cpu, DIFFTEST_REG_SIZE);
+  }
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
- // cpu_exec(n);
+//  assert(0);
+  cpu_exec(n);
 }
 
 void difftest_raise_intr(word_t NO) {
---
+++ npc/csrc/dut.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+void (*ref_difftest_memcpy)(uint32_t, void *buf, unsigned long n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+//void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+#define DIFFTEST_TO_REF 1
+
+
+// static bool is_skip_ref = false;
+// static int skip_dut_nr_inst = 0;
+
+// this is used to let ref skip instructions which
+// can not produce consistent behavior with NEMU
+// void difftest_skip_ref() {
+//   is_skip_ref = true;
+  // If such an instruction is one of the instruction packing in QEMU
+  // (see below), we end the process of catching up with QEMU's pc to
+  // keep the consistent behavior in our best.
+  // Note that this is still not perfect: if the packed instructions
+  // already write some memory, and the incoming instruction in NEMU
+  // will load that memory, we will encounter false negative. But such
+  // situation is infrequent.
+//   skip_dut_nr_inst = 0;
+// }
+
+// this is used to deal with instruction packing in QEMU.
+// Sometimes letting QEMU step once will execute multiple instructions.
+// We should skip checking until NEMU's pc catches up with QEMU's pc.
+// The semantic is
+//   Let REF run `nr_ref` instructions first.
+//   We expect that DUT will catch up with REF within `nr_dut` instructions.
+void difftest_skip_dut(int nr_ref, int nr_dut) {
+  skip_dut_nr_inst += nr_dut;
+
+  while (nr_ref -- > 0) {
+    ref_difftest_exec(1);
+  }
+}
+
+void init_difftest(char *ref_so_file, long img_size, int port) {
+  assert(ref_so_file != NULL);
+
+  void *handle;
+  handle = dlopen(ref_so_file, RTLD_LAZY | MUXNDEF(CONFIG_CC_ASAN, RTLD_DEEPBIND, 0));
+  assert(handle);
+
+  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  assert(ref_difftest_memcpy);
+
+  ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
+  assert(ref_difftest_regcpy);
+
+  ref_difftest_exec = dlsym(handle, "difftest_exec");
+  assert(ref_difftest_exec);
+
+  // ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+  // assert(ref_difftest_raise_intr);
+
+//   void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
+//   assert(ref_difftest_init);
+
+//   Log("Differential testing: %s", ASNI_FMT("ON", ASNI_FG_GREEN));
+//   Log("The result of every instruction will be compared with %s. "
+//       "This will help you a lot for debugging, but also significantly reduce the performance. "
+//       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
+
+//   ref_difftest_init(port);
+  ref_difftest_memcpy(0x80000000, gi_to_hi(0x80000000), img_size, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+}
+
+static void checkregs(CPU_state *ref, uint64_t pc) {
+  if (!isa_difftest_checkregs(ref, pc)) {
+    nemu_state.state = NEMU_ABORT;
+    nemu_state.halt_pc = pc;
+    isa_reg_display();
+  }
+}
+
+void difftest_step(uint64_t pc, uint64_t npc) {
+  CPU_state ref_r;
+
+  if (skip_dut_nr_inst > 0) {
+    ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+    if (ref_r.pc == npc) {
+      skip_dut_nr_inst = 0;
+      checkregs(&ref_r, npc);
+      return;
+    }
+    skip_dut_nr_inst --;
+    if (skip_dut_nr_inst == 0)
+      panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
+    return;
+  }
+
+  if (is_skip_ref) {
+    // to skip the checking of an instruction, just copy the reg state to reference design
+    ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+    is_skip_ref = false;
+    return;
+  }
+
+  ref_difftest_exec(1);
+  ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+
+  checkregs(&ref_r, pc);
+}
+// void init_difftest(char *ref_so_file, long img_size, int port) { }
+// #endif
