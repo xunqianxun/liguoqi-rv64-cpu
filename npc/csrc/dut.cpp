00cc4c5cfc172c8708d78db94691269968d4e58f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-18 20:23:21 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:21 up 13 days, 10:40,  1 user,  load average: 0.64, 0.47, 0.37

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -66,7 +66,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   handle = dlopen(ref_so_file, 0x00001);
 //  assert(handle);
 
-  ref_difftest_memcpy = void * dlsym(handle, "difftest_memcpy");
+  ref_difftest_memcpy =(void(*)(uint32_t, void *, unsigned long , bool)) dlsym(handle, "difftest_memcpy");
 //  assert(ref_difftest_memcpy);
 
   ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
