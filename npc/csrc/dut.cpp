26d924e24b8f050fa352d841476f53ba0f3f8a8d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 14:07:48 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:48 up 14 days,  4:25,  1 user,  load average: 0.79, 0.31, 0.16

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -161,7 +161,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, uint64_t pc) {
   return true;
 }
 
-static inline bool difftest_check_reg(const char *name, vaddr_t pc, word_t ref, word_t dut) {
+static inline bool difftest_check_reg(const char *name, uint64_t pc, uint64_t ref, uint64_t dut) {
   if (ref != dut) {
     printf("%s is different after executing instruction at pc = ""0x%016lx"
         ", right = " "0x%08x" ", wrong = " "0x%08x" ", diff = " "0x%08x",
