399a290958d23e5a7395f194d749476e258d0b24
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 21:03:42 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:03:42 up 14 days, 11:20,  1 user,  load average: 0.59, 0.58, 0.56

--- npc/vsrc/ex.v
+++ npc/vsrc/ex.v
@@ -74,7 +74,7 @@ end
 //    end
     
 //endfunction
-assign ex_pc_o = (rst == `ysyx22040228_RSTENA) ? `ysyx22040228_ZEROWORD : ex_pc_i ;
+assign ex_pc_o = (rst == `ysyx22040228_RSTENA) ? `ysyx22040228_ZEROWORD : pc_i ;
 
 reg      [`ysyx22040228_REGBUS]       exe_res   ;
 
