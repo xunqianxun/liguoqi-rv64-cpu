5c78cbf743413cb1075851252a24b0dc58c33807
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 16:47:55 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:47:55 up 16 days,  7:05,  1 user,  load average: 0.28, 0.24, 0.25

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -118,7 +118,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 //   ref_difftest_init(port);
   ref_difftest_memcpy(0x80000000, gi_to_hi(0x80000000), img_size, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-  printf("initial pc = %lx\n", &cpu.pc);
+ // printf("initial pc = %lx\n", &cpu.pc);
 }
 
 static void checkregs(CPU_state *ref, uint64_t pc) {
