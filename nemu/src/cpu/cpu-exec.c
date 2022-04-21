750bd60ea351a375eff875943817e5e8dfab9e73
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 15:15:53 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:15:53 up 16 days,  5:33,  1 user,  load average: 0.66, 0.56, 0.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,6 +31,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
+    printf("expc=%lx\n", pc);
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
