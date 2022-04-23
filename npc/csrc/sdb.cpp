c95a3dc5ef5e7d5798dbc653c98cce2b485261d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 13:48:08 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:48:08 up 18 days,  4:05,  1 user,  load average: 0.41, 0.21, 0.11

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -31,7 +31,7 @@ static char* rl_gets() {
     line_read = NULL;
   }
 
-  line_read = readline("(nemu) ");
+  line_read = readline("(npc) ");
 
   if (line_read && *line_read) {
     add_history(line_read);
--- npc/obj_dir/Vrvcpu.mk
+++ npc/obj_dir/Vrvcpu.mk
@@ -39,6 +39,7 @@ VM_USER_CFLAGS = \
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-ldl \
+	-lreadline \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
