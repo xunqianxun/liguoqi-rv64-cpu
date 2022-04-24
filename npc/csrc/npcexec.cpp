903d2f68657821a80effed9bfd08cd442b23b6f6
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 20:59:28 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:28 up 19 days, 11:16,  1 user,  load average: 0.43, 0.36, 0.36

--- npc/csrc/npcexec.cpp
+++ npc/csrc/npcexec.cpp
@@ -10,30 +10,30 @@ uint32_t ifetch(uint64_t addr, int len);
 // void exec_once();
 // int exec_close()
 int exit_exec_once;
-int  data_us    ;
+//int  data_us    ;
 
 void statistic();
 //bool exe_once_sign ;
 
-void exe_once(bool i ,bool exit){
-    if(i == 1) {
-    //exec_once();
-    data_us = 1;
-    }
-    if(exit == 1) {
-    //exec_close();
-    data_us = 0;
-    }
-}
+// void exe_once(bool i ,bool exit){
+//     if(i == 1) {
+//     //exec_once();
+//     data_us = 1;
+//     }
+//     if(exit == 1) {
+//     //exec_close();
+//     data_us = 0;
+//     }
+// }
 
 static void execute(uint64_t n) {
   int i;
   for (i=0; i<n; i++){
-    exe_once(1,0);
+    exe_once();
 //    difftest_step(rv64.pc, 1);
   }
   if(i == n){
-    exe_once(0,1);
+//    exe_once();
   }
 }  
 
@@ -53,32 +53,4 @@ void cpu_exec(uint64_t n) {
 void statistic(){
 }
 
-// void exec_once(){
-// while(1){
-// if(main_time > 10){
-//   rvcpu->rst = 0 ; 
-// }
-// if((main_time % 10) == 1){
-//   rvcpu->clk = 1;
-//   if(rvcpu->inst_addr >= 0x80000000){
-//   rvcpu->inst = ifetch(rvcpu->inst_addr, 4);
-//   }
-//   if(rv64.pc != 0 ){
-//     difftest_step(rv64.pc,1);
-//     exit_exec_once = 1;
-//   }
-  
-//   rvcpu->eval();
 
-// }
-// if(exit_exec_once == 1){
-//     break;
-// }
-// if((main_time % 10) == 6){
-//   rvcpu->clk = 0;
-// }
-//   rvcpu->eval();
-//   tfp->dump(main_time);
-//   main_time++;
-// }
-// }
