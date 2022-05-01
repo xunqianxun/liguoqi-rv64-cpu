58eb5c697fffd7206917cfcde1f73cb27475cedd
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 18:46:32 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:46:32 up 4 days, 22:37,  1 user,  load average: 0.38, 0.23, 0.08

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <nemu.h>
 #include <time.h>
+#include <stdio.h>
 
 void __am_timer_init() {
 
@@ -8,6 +9,7 @@ void __am_timer_init() {
 struct timespec nows;
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = nows.tv_sec * 1000000 + nows.tv_nsec / 1000;
+  printf("%d",nows.tv_sec);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
