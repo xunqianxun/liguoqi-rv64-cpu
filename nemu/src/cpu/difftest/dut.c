7e326b9b6a5d04b7706708454d05ee23dc2c9f85
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 21:54:59 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:59 up 11 days,  9:24,  1 user,  load average: 0.70, 0.54, 0.52

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -78,8 +78,8 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
-    npc_state.state = NEMU_ABORT;
-    npc_state.halt_pc = pc;
+    nemu_state.state = NEMU_ABORT;
+    nemu_state.halt_pc = pc;
     isa_reg_display();
   }
 }
