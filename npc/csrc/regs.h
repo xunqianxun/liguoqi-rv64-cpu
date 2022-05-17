bdeb4edb95fabf737a98f9efbd9071abef8edc4d
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-17 21:45:49 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:49 up 11 days,  9:15,  1 user,  load average: 1.46, 0.83, 0.57

--- npc/csrc/regs.h
+++ npc/csrc/regs.h
@@ -6,8 +6,8 @@
 //#include "/home/mulin/ysyx-workbench/nemu/include/utils.h"
 
 enum { NEMU_RUNNING, NEMU_STOP, NEMU_END, NEMU_ABORT, NEMU_QUIT };
-extern void isa_exec_once();
-extern int close_npc(int y);
+extern void isa_exec_once(int y);
+extern int close_npc();
 bool difftest_ena;
 
 typedef struct 
