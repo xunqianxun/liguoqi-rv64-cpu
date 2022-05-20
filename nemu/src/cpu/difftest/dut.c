57667bfdc41f087ab7eaf3475ecddd147c5897e0
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-20 20:23:36 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:36 up 14 days,  7:53,  1 user,  load average: 0.70, 0.37, 0.17

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -108,6 +108,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   ref_difftest_exec(1);
+  printf("sssssssssssssssssssssssssss\n");
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, pc);
