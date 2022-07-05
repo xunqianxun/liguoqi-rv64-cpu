dc380771457afc116278ad6d42297396672ff030
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 11:23:14 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:23:14 up 20 days,  3:34,  1 user,  load average: 0.38, 0.37, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -205,8 +205,8 @@ while(ddy){
     rvcpu->clk = 1;
     rvcpu->eval();
     if(rvcpu->read_ena_sign_ == 1){
-      //printf("read\n");
-      //printf("addr == %lx\n",rvcpu->out_addr_outp);
+      printf("read\n");
+      printf("addr == %lx\n",rvcpu->out_addr_outp);
       if(rvcpu->out_addr_outp != 0){
 
       rvcpu->read_data_sign_ = vaddr_read(rvcpu->out_addr_outp, 8);
