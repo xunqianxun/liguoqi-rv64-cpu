f83fffb7fd47dbf4c8a2611be1d3b94f635af936
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-06 09:35:06 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:35:06 up 21 days,  1:46,  1 user,  load average: 0.47, 0.49, 0.50

--- npc/vsrc/divider.v
+++ npc/vsrc/divider.v
@@ -66,11 +66,8 @@ module divider (
                 end 
             end
             else  begin
-                // shang_data <= temp_a[63 :0 ] ;
-                // yushu_data <= temp_a[127:64] ;
                 counter <= 8'b0 ;
-                // temp_a <= {`ysyx22040228_ZEROWORD,op1_absolute};
-                // temp_b <= {op2_absolute,`ysyx22040228_ZEROWORD};
+                finish_sign <= 1'b0 ;
                 if(div_ready)begin
                     finish_sign <= 1'b1 ;
                 end 
