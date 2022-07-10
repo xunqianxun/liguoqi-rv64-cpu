be50bfcdf1108a9048c2193a30879f84556dcc97
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-10 13:44:16 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:44:16 up  3:42,  1 user,  load average: 0.35, 0.35, 0.28

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -4,7 +4,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
+  
   return 0;
 }
 
