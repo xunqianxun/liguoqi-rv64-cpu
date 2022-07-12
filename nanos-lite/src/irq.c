f669e3320a374037735c73ef9abf29a828a72654
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 19:32:29 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:32:29 up 22:42,  1 user,  load average: 0.65, 0.45, 0.28

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD : printf("yelid ecall\n"); break;
+    case EVENT_YIELD : printf("yelid ecall\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
