12a28264dd24ebc1c7651fb9b1324c1e927f4961
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 14:08:54 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:08:54 up 17:18,  1 user,  load average: 0.20, 0.17, 0.25

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -9,9 +9,9 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     for (i = 1; i < ARRLEN(cpu.gpr); i ++) {
       difftest_check_reg(reg_name(i, 4), pc, ref_r->gpr[i], cpu.gpr[i]);
     }
-    difftest_check_reg("mrpc"  , pc, ref_r->mepc, cpu.mepc);
+    difftest_check_reg("mepc"  , pc, ref_r->mepc, cpu.mepc);
     difftest_check_reg("mcause", pc, ref_r->mcause, cpu.mcause);
-    difftest_check_reg("mrpc"  , pc, ref_r->mtvec, cpu.mtvec);
+    difftest_check_reg("mtvec"  , pc, ref_r->mtvec, cpu.mtvec);
     difftest_check_reg("pc"    , pc, ref_r->pc, cpu.pc);
     return false;
   }
