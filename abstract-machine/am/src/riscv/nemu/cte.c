f7059a93754ff11f5acf400245e450af1bdc2fd4
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-12 16:07:59 +0800

    >  compile NEMU ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:07:59 up 19:17,  1 user,  load average: 0.30, 0.45, 0.36

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-        case 0 : ev.event = EVENT_YIELD ; break;
+        case 11 : ev.event = EVENT_YIELD ; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
