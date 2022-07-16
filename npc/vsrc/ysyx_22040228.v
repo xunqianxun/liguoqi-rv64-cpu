866758bb110cf5f117b1634492882fb0d18e9d4a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-16 11:28:07 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:28:07 up 4 days, 14:37,  1 user,  load average: 0.40, 0.31, 0.36

--- npc/vsrc/ysyx_22040228.v
+++ npc/vsrc/ysyx_22040228.v
@@ -261,7 +261,6 @@ module ysyx_22040228 (
     /* verilator lint_off UNUSED */
     wire   [`ysyx22040228_PROT_BUS]  tim_axi_aw_port  ;
     wire   [`ysyx22040228_QOS_BUS]   tim_axi_aw_qos   ;
-    /* verilator lint_on UNUSED */
     wire                             tim_axi_aw_ready ;
     wire                             tim_axi_aw_valid ;
 
@@ -334,8 +333,6 @@ module ysyx_22040228 (
     wire                             io_axi_r_last   ;
     wire                             io_axi_r_valid  ;
     wire                             io_axi_r_ready  ;
-    /* verilator lint_on UNDRIVEN */
-    /* verilator lint_on UNUSED */
 
     assign  {soc_axi_aw_id     , tim_axi_aw_id     , io_axi_aw_id     }   =  add_axi_aw_id   ;
     assign  {soc_axi_aw_addr   , tim_axi_aw_addr   , io_axi_aw_addr   }   =  add_axi_aw_addr ;
