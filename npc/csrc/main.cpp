9dad0f8bc8b2f62e98c926d76bc744074fb4679f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-17 16:13:18 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:18 up 5 days, 19:22,  1 user,  load average: 0.52, 0.53, 0.61

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -35,10 +35,9 @@ tfp->open("obj_dir/ysyxSoCFull.vcd") ; // open vcd
 char *path = (char *)"/home/mulin/ysyxSoC/ysyx/program/bin/loader/hello-loader.bin";
 flash_init(path);
 
-while((main_time > 100000) && !Verilated::gotFinish()){
-	  if( main_time % 10 == 0 ) {ysyxSoCFull->clock = 0; printf("inst_pc = 0x%lx inst_data = %x\n", pc_addr, pc_inst);}
-	  if( main_time % 10 == 5 ) {ysyxSoCFull->clock = 1;}
-		printf("inst_pc = 0x%lx inst_data = %x\n", pc_addr, pc_inst);  
+while((main_time < 100000) && !Verilated::gotFinish()){
+	  if( main_time % 10 == 0 ) {ysyxSoCFull->clock = 0;}
+	  if( main_time % 10 == 5 ) {ysyxSoCFull->clock = 1;} 
 	  if( main_time < 10 )
 	  {
 		ysyxSoCFull->reset = 1;
