03046d1d0efb7e667e7f0d77a477c9a1086260f8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 15:58:31 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:58:31 up 8 days, 19:08,  1 user,  load average: 0.25, 0.31, 0.39

--- npc/vsrc/uncache_mmio.v
+++ npc/vsrc/uncache_mmio.v
@@ -2,6 +2,14 @@
 `include "defines_axi4.v"
 `include "cache_defines.v"
 /* verilator lint_off LATCH */
+`define AXI_SIZE_BYTES_1                                    3'b000
+`define AXI_SIZE_BYTES_2                                    3'b001
+`define AXI_SIZE_BYTES_4                                    3'b010
+`define AXI_SIZE_BYTES_8                                    3'b011
+`define AXI_SIZE_BYTES_16                                   3'b100
+`define AXI_SIZE_BYTES_32                                   3'b101
+`define AXI_SIZE_BYTES_64                                   3'b110
+`define AXI_SIZE_BYTES_128                                  3'b111
 module uncache_mmio (
     input     wire                                         clk              ,
     input     wire                                         rst              ,
@@ -23,6 +31,7 @@ module uncache_mmio (
     output    wire          [7:0]                          arb_mask         ,
     output    wire                                         arb_we           ,
     output    wire                                         arb_re           ,
+    output    wire          [2:0]                          arb_size_data    ,
     input     wire          [63:0]                         in_arb_data      ,
     input     wire                                         in_arb_finish    ,
 
@@ -55,9 +64,20 @@ module uncache_mmio (
     assign dcache_re   = ~uncache ? core_re   : `ysyx22040228_ENABLE   ;
 
  
+    wire [2:0]  byte_size ;
+    assign      byte_size   = (core_we && (core_we_type == 3'b000)) || (core_re && (core_re_type == 3'b000)) ? `AXI_SIZE_BYTES_1 :
+                              (core_we && (core_we_type == 3'b001)) || (core_re && (core_re_type == 3'b000)) ? `AXI_SIZE_BYTES_2 : 
+                              (core_we && (core_we_type == 3'b010)) || (core_re && (core_re_type == 3'b000)) ? `AXI_SIZE_BYTES_4 :
+                              (core_we && (core_we_type == 3'1000)) || (core_re && (core_re_type == 3'b000)) ? `AXI_SIZE_BYTES_8 :
+                                                                                                               `AXI_SIZE_BYTES_1 ;
+    wire [63:0] addr_outaxi ;
+    assign      addr_outaxi = (core_we && (core_we_type == 3'b000)) || (core_re && (core_re_type == 3'b000)) ? core_addr                 :
+                              (core_we && (core_we_type == 3'b001)) || (core_re && (core_re_type == 3'b000)) ? {core_addr[63:1], 1'b0}   : 
+                              (core_we && (core_we_type == 3'b010)) || (core_re && (core_re_type == 3'b000)) ? {core_addr[63:2], 2'b00}  :
+                              (core_we && (core_we_type == 3'1000)) || (core_re && (core_re_type == 3'b000)) ? {core_addr[63:3], 3'b000} :
+                                                                                                               core_addr                 ;                                                                                                          
 
-    //---------------------------uncache_set----------------------------//
-
+    /*
     reg   [1:0]     out_counter ;
     reg   [1:0]     out_counter_n ;
     reg   [63:0]    uncache_temp ;
@@ -156,6 +176,59 @@ module uncache_mmio (
     assign  in_core_finish  = (uncache && core_re) ? uncahche_read_finish :
                               (uncache && core_we) ? uncahche_write_finish:
                                                          in_dcache_finish ;    
+    */   
+
+
+    reg   [63:0]    uncache_temp        ;
+    reg             uncahche_read_finish;
+    reg             uncache_out_ena  ;
+    reg   [63:0]    uncahce_out_addr ;
+    reg   [2:0]     uncache_out_size ;
+
+    always @(*) begin
+        if((uncache && core_re) && (~in_arb_finish)) begin
+            uncache_out_ena   = `ysyx22040228_ABLE ;
+            uncahce_out_addr  =  addr_outaxi       ;
+        end 
+        else if((core_re) && (uncache) && (in_arb_finish)) begin
+            uncache_out_ena   = `ysyx22040228_ENABLE  ;
+            uncahce_out_addr  = `ysyx22040228_ZEROWORD;
+            uncache_temp      = in_arb_data           ;
+            uncahche_read_finish = `ysyx22040228_ABLE ;
+        end  
+        else begin
+            uncahche_read_finish = `ysyx22040228_ENABLE; 
+        end
+    end 
+
+    reg              uncache_out_ena1 ;
+    reg   [63:0]     uncache_out_addr1 ;
+    reg              uncahche_write_finish;
+    always @(*) begin
+        if((uncache && core_we) && (~in_arb_finish)) begin
+            uncache_out_ena1   = `ysyx22040228_ABLE ;
+            uncahce_out_addr1  =  addr_outaxi       ;
+        end 
+        else if((uncache) && (in_arb_finish) && (core_we)) begin
+            uncache_out_ena1   = `ysyx22040228_ENABLE   ;
+            uncahce_out_addr1  = `ysyx22040228_ZEROWORD ;
+            uncahche_write_finish = `ysyx22040228_ABLE  ;
+        end 
+        else begin
+            uncahche_write_finish = `ysyx22040228_ENABLE;
+        end
+    end 
+
+    assign  in_core_data    = uncache ? uncache_temp  : in_dcache_data   ;
+    
+    assign  in_core_finish  = (uncache && core_re) ? uncahche_read_finish :
+                              (uncache && core_we) ? uncahche_write_finish:
+                                                         in_dcache_finish ; 
+    assign  arb_addr =  (uncache && core_re) ? uncahce_out_addr :
+                        (uncache && core_we) ? uncache_out_addr1:
+                                          `ysyx22040228_ZEROWORD; 
+    assign  arb_mask = uncache ? core_mask : 8'b00000000           ;
+    assign  arb_we   = uncache ? uncache_out_ena1   : `ysyx22040228_ENABLE  ;
+    assign  arb_re   = uncache ? uncache_out_ena    : `ysyx22040228_ENABLE  ;
 
 endmodule
-
