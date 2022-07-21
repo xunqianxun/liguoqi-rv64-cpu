ab4b88816ac152a2702690b21da76e6c540016dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-21 16:16:13 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux mulin-XPS-13-7390 5.15.0-41-generic #44~20.04.1-Ubuntu SMP Fri Jun 24 13:27:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:13 up  6:00,  1 user,  load average: 0.98, 0.54, 0.51

--- npc/vsrc/arbitratem.v
+++ npc/vsrc/arbitratem.v
@@ -421,7 +421,7 @@ module arbitratem (
             dwrite_aw_valid     <= `ysyx22040228_ENABLE;
             dwrite_w_valid      <= `ysyx22040228_ABLE;
             write_wshan         <= `ysyx22040228_ABLE  ;
-            dwrite_w_data       <= d_cache_data      ;
+            dwrite_w_data       <= 64'd12345678      ;
             dwrite_w_strb       <= 8'b11111111       ;
             dwrite_w_last       <= `ysyx22040228_ABLE;
         end 
@@ -434,7 +434,7 @@ module arbitratem (
         end 
         else if(arbitrate_state == `ysyx22040228_ARB_DWRITE) begin
             dwrite_aw_id        <= 4'b0001           ;
-            dwrite_aw_addr      <= {d_cache_addr[63:2], 2'b00} ;
+            dwrite_aw_addr      <= {d_cache_addr[63:3], 3'b00} ;
             dwrite_aw_len       <= 8'd0              ;
             dwrite_aw_size      <= `AXI_SIZE_BYTES_8 ;
             dwrite_aw_burst     <= `AXI_BURST_TYPE_INCR;
