f81034c5683c359b85fd040eaf8d8c2ed04dd868
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 16:55:19 +0800

    >  sim RTL ysyx_22040228 Li Guo Qi Linux guoqili-xps-13 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:19 up 10:16,  1 user,  load average: 2.13, 1.11, 0.57

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -32,7 +32,7 @@ Verilated::traceEverOn(true) ; //out vcd need
 tfp = new VerilatedVcdC ; // out vcd need
 ysyxSoCFull->trace(tfp,0) ;
 tfp->open("obj_dir/ysyxSoCFull.vcd") ; // open vcd
-char *path = (char *)"/home/mulin/ysyx-workbench/npc/bin/loader/memtest-loader.bin";
+char *path = (char *)"/home/mulin/ysyx-workbench/bin/rt-thread/flash/rtthread.bin";
 flash_init(path);
 
 while((1) && !Verilated::gotFinish()){
